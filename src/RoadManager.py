796cb3d601464772ad02a384fb69e9a0e190fe73
IanChar <ianchar4@gmail.com>
2017-01-20 17:19:52 -0700

    Added logic for booths

--- src/RoadManager.py
+++ src/RoadManager.py
@@ -8,8 +8,8 @@ class RoadManager(object):
     def __init__(self, lane_sizes, target_lanes, rate_in, rate_out):
         self.road = Road(lane_sizes, target_lanes)
         self.actors = []
-        self.booths = [TollBooth(rate_in, rate_out)
-                       for _ in range(len(lane_sizes))]
+        self.booths = [TollBooth(lane_num, rate_in, rate_out)
+                       for lane_num in range(len(lane_sizes))]
 
     def tick(self, print_progress=False):
         # Make all the actors act.
@@ -19,12 +19,12 @@ class RoadManager(object):
                 to_remove.append(actor_id)
         throughput = self.road.commit_updates()
         # Remove all actors that have left the scene.
-        for actor_id in to_remove:
+        for actor_id in to_remove[::-1]:
             del self.actors[actor_id]
         # Check if any actors have entered the scene.
         to_add = []
         for lane_num, booth in enumerate(self.booths):
-            if booth.tick():
+            if booth.tick(self.road):
                 to_add.append(lane_num)
         for lane_num in to_add:
             # TODO: Alter this if we want different cars.
